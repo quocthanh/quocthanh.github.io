ab2a1eb36d79af870addac883d780b1fac3b5d54
quocthanh <quocthanh2608@yahoo.com>
2016-08-22 16:07:53 +0700

    bai tap php: bai 1

--- PHP/basic/bai tap/bai1.php
+++ PHP/basic/bai tap/bai1.php
@@ -39,6 +39,7 @@
             die();
         }
 
+        //find number 1 in array 1
         echo '<b>Cau 1:</b><br>';
         if (in_array(1, $array1)) {
             echo 'Found';
@@ -46,19 +47,23 @@
             echo 'Not found';
         }
 
+        //merge array 2 and array 3 => arrayM
         echo '<br><b>Cau 2:</b><br>';
         $arrayM = array_unique(array_merge($array2, $array3));
         echo implode(', ', $arrayM);
 
+        //print values of (*) (arrayM) whose sum of digits divide 2 = 0
         echo '<br><b>Cau 3:</b><br>';
         $array_tmp = array_filter($arrayM, 'divide2');
         echo implode(', ', $array_tmp);
 
+        //print values of array 1 that exist in (*) (arrayM) in ascending order
         echo '<br><b>Cau 4:</b><br>';
         $array_tmp = array_intersect($array1, $arrayM);
         sort($array_tmp);
         echo implode(', ', $array_tmp);
 
+        //print values of array 1 whose keys is not in (*) in descending order
         echo '<br><b>Cau 5:</b><br>';
         $array_tmp = array_flip($arrayM);
         $array_tmp = array_diff_key($array1, $array_tmp);
