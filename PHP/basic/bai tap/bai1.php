8b6e45a23ff88e93001b22a2c6bf31242fd4cb12
quocthanh <quocthanh2608@yahoo.com>
2016-08-22 18:06:48 +0700

    bai tap php: bai 1

--- PHP/basic/bai tap/bai1.php
+++ PHP/basic/bai tap/bai1.php
@@ -10,7 +10,7 @@
  * @param array $array2
  * @param array $array3
  *
- * @throws InvalidArgumentException if the arguments is not of type 'array'
+ * @throws LogicException if the arguments is not of type 'array'
  *
  * @return void
  *
@@ -18,34 +18,26 @@
     function arrayPractice($array1, $array2, $array3)
     {
         try {
-            $inValid = [];
             $argList = func_get_args();
-            foreach ($argList as $key => $value) {
-                if (!is_array($value)) {
-                    $inValid[] = $key + 1;
+
+            $invalidParameters = array_filter($argList, 'isNotArray');
+            if (count($invalidParameters) > 0) {
+                $msg = 'Invalid parameter';
+                foreach ($invalidParameters as $key => $value) {
+                    $msg .= ' ' . ($key + 1) . ',';
                 }
-            }
-            if (count($inValid) > 0) {
-                $txt = 'Invalid parameter';
-                foreach ($inValid as $value) {
-                    $txt .= ' ' . $value . ',';
-                }
-                $txt = substr($txt, 0, -1);
-                throw new Exception($txt);
+                $msg = substr($msg, 0, -1);
+                throw new LogicException($msg);
             }
         }
-        catch(Exception $e) {
+        catch (Exception $e) {
             echo '<b>LogicException:</b> ' . $e->getMessage();
             die();
         }
 
         //find number 1 in array 1
         echo '<b>Cau 1:</b><br>';
-        if (in_array(1, $array1)) {
-            echo 'Found';
-        } else {
-            echo 'Not found';
-        }
+        echo in_array(1, $array1) ? 'Found' : 'Not Found';
 
         //merge array 2 and array 3 => arrayMerge
         echo '<br><b>Cau 2:</b><br>';
@@ -54,7 +46,7 @@
 
         //print values of (*) (arrayMerge) whose sum of digits divide 2 = 0
         echo '<br><b>Cau 3:</b><br>';
-        $modTwo = array_filter($arrayMerge, 'divide2');
+        $modTwo = array_filter($arrayMerge, 'isSumOfDigitsEven');
         echo implode(', ', $modTwo);
 
         //print values of array 1 that exist in (*) (arrayMerge) in ascending order
@@ -71,22 +63,35 @@
         echo implode(', ', $diffKey);
     }
 /**
- * test if $num divide 2 = 0
+ * return true if sun of digits is even
+ * else, return false.
  *
  * @param int $num
  *
- * @return boolean return true if $num divide 2 = 0
+ * @return boolean
  *
  */
-    function divide2($num)
+    function isSumOfDigitsEven($num)
     {
-        if ((array_sum(str_split($num))) % 2 == 0) {
-            return true;
-        }
+        return array_sum(str_split($num)) % 2 === 0;
+    }
+/**
+ * return true if input is not a array
+ *
+ * @param mixed $input
+ *
+ * @return bool
+ */
+    function isNotArray($input)
+    {
+        return !is_array($input);
     }
 
     arrayPractice([1, -3, 0, 10], [1, 2, 3], [3, 11, 5, 7]);
     echo '<br><br>';
     arrayPractice([0, -3, 0, 10, 12], [1, 9, -3, 51], [33, 11, 9, 5, 7]);
+    echo '<br><br>';
+    $arr1 = [1, 2, 3];
+    arrayPractice($arr1, [], []);
     echo '<br><br>';
     arrayPractice(1, [1, 2, 3], 'poke');
