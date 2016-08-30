476e6bc4a2409fabf867bac6a35fd08237911df4
quocthanh <quocthanh2608@yahoo.com>
2016-08-30 13:54:43 +0700

    bai tap PHP: fix code lan 5

--- PHP/basic/AJAX_JSON/receive.php
+++ PHP/basic/AJAX_JSON/receive.php
@@ -30,8 +30,5 @@ $response = array(
 );
 
 $response = json_encode($response);
-if (json_last_error() !== JSON_ERROR_NONE) {
-    throw new LogicException('JSON error: ' . json_last_error_msg());
-}
 
 echo $response;
